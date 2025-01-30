473f33c72bf0afa2d94cd1604f4701646d259774
Andres Cabada <cm_andres@hotmail.com>
2025-01-30 17:59:28 -0600

    Render Test

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,23 +1,13 @@
-function isAuthenticated(options = {}) {
-    return function(req, res, next) {
-        if (typeof req.isAuthenticated !== 'function') {
-            return res.status(500).json({ message: "Authentication middleware not configured correctly" });
-        }
+function isAuthenticated(req, res, next) {
+    console.log("Session User:", req.session.user);
+    console.log("Passport User:", req.user);
 
-        if (req.isAuthenticated()) {
-            return next();
-        }
+    if (req.isAuthenticated()) { 
+        return next();
+    }
 
-        if (options.redirectTo) {
-            return res.redirect(options.redirectTo);
-        }
-
-        if (req.accepts('json')) {
-            res.status(401).json({ message: options.message || "Unauthorized - Please log in" });
-        } else {
-            res.status(401).send('Unauthorized - <a href="/login">Please log in</a>');
-        }
-    };
+    res.status(401).json({ message: "Unauthorized - Please log in" });
 }
 
-module.exports = isAuthenticated;
+module.exports = { isAuthenticated };
+
